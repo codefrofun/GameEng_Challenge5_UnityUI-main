b167b52ed2f96255c96f31812a174aaa0e3e7db9
codefrofun <blackpinkaricat@gmail.com>
2025-02-05 21:57:12 -0400

    Added all assets, created scene/gameview, organized hierarchy
    Need to program script

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int ScoreCount;
+    public TextMeshProUGUI ScoreText;
+    private Button Button;
+
+    public void ScoreUpdate(int score)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddToScore()
     {
-        
+
+    }
+
+    public void DecreaseScore()
+    {
+
+    }
+
+    public void ResetScore()
+    {
+        {
+
+        }
     }
 }
